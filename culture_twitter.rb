30428bbb12d90b4a80dda8e266f500398389f320
福重 伸太朗 <japanrock@fukushige-shintarou-no-iMac.local>
2010-04-26 11:15:09 +0900

    TwitterBaseクラスを外部ファイルで管理することにした。ymlのロードをFile.dirname(__FILE__)を使って、引数で取るのをやめた。

--- culture_twitter.rb
+++ culture_twitter.rb
@@ -8,72 +8,29 @@ require 'hpricot'
 require 'open-uri'
 require 'yaml'
 require 'parsedate'
-require "kconv"
-
-### TODO:
-###  ・TwitterBaseクラスはあとで外に出す
+require 'kconv'
+require File.dirname(__FILE__) + '/twitter_oauth'
 
 # Usage:
-# ruby lrhert_twitter.rb /path/to/sercret_key.yml /path/to/lrhert.yml
-
-
-# TwitterのAPIとのやりとりを行うクラス
-class TwitterBase
-  def initialize
-    # config.yml内のsercret_keys.ymlをloadします。
-    @secret_keys = YAML.load_file(ARGV[0] || 'sercret_key.yml')
-  end
-  
-  def consumer_key
-    @secret_keys["ConsumerKey"]
-  end
-
-  def consumer_secret
-    @secret_keys["ConsumerSecret"]
-  end
-
-  def access_token_key
-    @secret_keys["AccessToken"]
-  end
-
-  def access_token_secret
-    @secret_keys["AccessTokenSecret"]
-  end
-
-  def consumer
-    @consumer = OAuth::Consumer.new(
-      consumer_key,
-      consumer_secret,
-      :site => 'http://twitter.com'
-    )
-  end
-
-  def access_token
-    consumer
-    access_token = OAuth::AccessToken.new(
-      @consumer,
-      access_token_key,
-      access_token_secret
-    )
-  end
-
-  def post(tweet=nil)
-    response = access_token.post(
-      'http://twitter.com/statuses/update.json',
-      'status'=> tweet
-    )
-  end
-end
+#  1. このファイルと同じディレクトリに以下３つのファイルを設置します。
+#   * twitter_oauth.rb
+#    * http://github.com/japanrock/TwitterTools/blob/master/twitter_oauth.rb
+#   * sercret_key.yml 
+#    * http://github.com/japanrock/TwitterTools/blob/master/secret_keys.yml.example
+#   * culture.yml
+#    * http://github.com/japanrock/TwitterLR_Culture/blob/master/culture.yml
+#  2. このファイルを実行します。
+#   ruby lrhert_twitter.rb
 
 class LrCulture
   attr_reader :selected_culture
   attr_reader :select
 
   def initialize
-    @culture = YAML.load_file(ARGV[1] || 'lrhert.yml')
+    # カレントディレクトリの culture.yml をロードします
+    @culture = YAML.load_file(File.dirname(__FILE__) + '/culture.yml')
   end
 
-  # ここはあとでリファクタリング・・・微妙なので・・・
   def head
     ""
   end
@@ -88,12 +45,13 @@ class LrCulture
   end
 end
 
-twitter_base = TwitterBase.new
-lr_culture   = LrCulture.new
+twitter_oauth = TwitterOauth.new
+lr_culture    = LrCulture.new
 
 content  = lr_culture.random_select
 head     = lr_culture.head
 url      = lr_culture.selected_culture["url"]
 contents = lr_culture.selected_culture["contents"]
 
-twitter_base.post(head + contents + " - " + url)
+twitter_oauth.post(head + contents + " - " + url)
+puts head + contents + " - " + url
